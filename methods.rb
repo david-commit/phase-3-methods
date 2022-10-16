22d5423f21e789c5d3155f1877e3619c35f6b62d
david-commit <david.ondiege@students.moringaschool.com>
2022-10-16 22:09:38 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+
+def greet_programmer
+ puts "Hello, programmer!"
+end
+
+def greet name
+ puts "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer"
+ puts "Hello, #{name}!"
+end
+
+def add a, b
+ a + b
+end
+
+def halve input
+ if input.class != Integer
+  nil
+ else
+  input / 2
+ end
+end
